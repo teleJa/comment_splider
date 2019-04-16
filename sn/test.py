9ead5b4ce3761641f32408d3544c0a77445a2b53
teleJa <48516499+teleJa@users.noreply.github.com>
2019-04-16 21:27:23 +0800

    Delete test.py

--- sn/test.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# @author: Tele
-# @Time  : 2019/04/15 下午 10:17
-import re
-test = "//review.suning.com/cluster_cmmdty_review/general-30259269-000000010748901691-0000000000-1-total.htm?originalCmmdtyType=general"
-res = re.findall("general-(.*?)-",test)
-
-print(res[0])
